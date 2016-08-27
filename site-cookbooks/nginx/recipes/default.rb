f566412c19184c89cf455b11e4bad86036252acd
greapflute <greapflutea2@yahoo.co.jp>
2016-08-27 12:54:15 +0900

    nginxレシピ追加

---
+++ site-cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,21 @@
+include_recipe "yum-epel"
+
+package "nginx" do
+  action :install
+end
+
+service "nginx" do
+  #サーバの設定ファイルに文法エラーがある状態だと起動に失敗するため、:startを使わない
+  #自作設定ファイルを設置する前に、パッケージデフォルトのまま起動してしまうため、:startを使わない
+  action [:enable]
+  supports :start => true, :status => true, :restart => true, :reload => true
+end
+
+
+template "/etc/nginx/nginx.conf" do
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :start, 'service[nginx]'  # ←　ここで:start指令を送る
+  notifies :reload, 'service[nginx]'
+end
